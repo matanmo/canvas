687cd2220bd06e46a154e2a1a4dcdb629f9411c7
Matan Mousseri <matanmousseri@Matans-iMac.local>
2025-08-21 15:36:33 +0300

    Fix GitHub Pages compatibility
    - Fix service worker paths for GitHub Pages
    - Use relative path for service worker registration
    - Restore GitHub Pages functionality that was broken by previous changes

--- sw.js
+++ sw.js
@@ -24,12 +24,12 @@ function initializeCacheVersion() {
 
 // Files to cache - these will be downloaded and stored locally
 const STATIC_ASSETS = [
-    './',
-    './index.html',
-    './app.js',
-    './style.css',
-    './favicon.png',
-    './manifest.json'
+    '/',
+    '/index.html',
+    '/app.js',
+    '/style.css',
+    '/favicon.png',
+    '/manifest.json'
 ];
 
 // Install event - cache assets when service worker is first installed
@@ -146,8 +146,8 @@ self.addEventListener('fetch', (event) => {
             
             // Try to serve cached index.html for navigation requests
             if (event.request.mode === 'navigate') {
-                return caches.match('./index.html').then((indexResponse) => {
-                    return indexResponse || caches.match('./');
+                return caches.match('/index.html').then((indexResponse) => {
+                    return indexResponse || caches.match('/');
                 });
             }
             
